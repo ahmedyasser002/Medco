591e0e6603119cc963bc1d8290dca210ba2da6d6
ahmedyasser002 <ay012941@gmail.com>
2025-05-27 00:05:29 +0300

    Appointments

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser , loginUser, getProfile, updateProfile, getPatientList } from '../controllers/userController.js'
+import { registerUser , loginUser, getProfile, updateProfile, getPatientList, bookAppointment, listAppointment, cancelAppointment } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import authAdmin from '../middlewares/authAdmin.js'
 import upload from '../middlewares/multer.js'
@@ -11,6 +11,9 @@ userRouter.post('/login',loginUser)
 userRouter.get('/get-profile' , authUser , getProfile)
 userRouter.post('/update-profile' , upload.single('image'),authUser, updateProfile )
 userRouter.get('/list' , authAdmin , getPatientList)
+userRouter.post('/book-appointment' , authUser , bookAppointment)
+userRouter.get('/list-appointments' , authUser , listAppointment)
+userRouter.post('/cancel-appointment' , authUser , cancelAppointment)
 
 
 
