95ebb89c097665d5a938df3feaf250e236096a99
ahmedyasser002 <ay012941@gmail.com>
2025-05-29 01:07:09 +0300

    update list appointments by patient id

--- routes/appointmenrRoute.js
+++ routes/appointmenrRoute.js
@@ -5,11 +5,13 @@ import {
   listAppointmentByPatientId,
 } from "../controllers/appointmentController.js";
 import { allowedTo, protect } from "../middlewares/auth-middleware.js";
+import patientModel from "../models/patientModel.js";
+import appointmentModel from "../models/appointmentModel.js";
 
 const appointmentRouter = express.Router();
 
 appointmentRouter.get("/doctor", protect(), allowedTo('doctor'), listAppointmentByDoctorId);
-appointmentRouter.get("/patient", protect(), listAppointmentByPatientId);
+appointmentRouter.get("/patient", protect(patientModel), listAppointmentByPatientId);
 appointmentRouter.post("/book",bookAppointment)
 
 export default appointmentRouter;
