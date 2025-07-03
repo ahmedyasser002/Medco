c636535a92d38037e86314a8de4e084c9098ecf3
ahmedyasser002 <ay012941@gmail.com>
2025-07-04 02:40:41 +0300

    laboratory

--- routes/labRoute.js
+++ routes/labRoute.js
@@ -7,7 +7,7 @@ const labRouter = express.Router();
 labRouter.get("/get-lab" , protect() , allowedTo("doctor") , getAllLaboratories);
 labRouter.get("/get-tests", protect() , allowedTo("laboratory") , getTests);
 labRouter.post("/add-test", protect() , allowedTo("doctor") , addTest);
-labRouter.patch("complete-test", protect() , allowedTo("laboratory") , completeTest);
+labRouter.patch("/complete-test", protect() , allowedTo("laboratory") , completeTest);
 
 
 export default labRouter;
